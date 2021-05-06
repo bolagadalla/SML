b1782c9415e4970b727111d060ab9ce51263966e
Bola Gadalla <bolagadalla777@gmail.com>
2021-05-06 18:04:26 -0400

    Made auth functions to be async

--- src/auth.rs
+++ src/auth.rs
@@ -2,11 +2,18 @@ use serde::{Deserialize, Serialize};
 use std::fs::OpenOptions;
 use std::io::{self, Write};
 use std::path::PathBuf;
+use async_recursion::async_recursion;
 
 #[derive(Serialize, Deserialize)]
 pub struct User {
     pub name: String,
     pub token: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct UserLogin {
+    pub email: String,
+    pub password: String,
 }
 
 impl From<PathBuf> for User {
@@ -21,7 +28,9 @@ impl From<PathBuf> for User {
     }
 }
 
-pub fn handle_auth() -> Option<User> {
+/// Gets the credentials from the user
+fn get_credentials() -> UserLogin
+{
     let mut email: String = "".to_string();
 
     print!("Log in to mojang\nEmail: ");
@@ -33,15 +42,33 @@ pub fn handle_auth() -> Option<User> {
 
     let password: String = rpassword::prompt_password_stdout("Password: ").unwrap();
 
-    let user = authenticate(email.as_str(), password.as_str());
-    if user.is_none() {
-        handle_auth()
-    } else {
-        Some(user.unwrap())
+    UserLogin {
+        email : email,
+        password : password,
     }
 }
 
-pub fn authenticate(email: &str, password: &str) -> Option<User> {
+#[async_recursion] // This transforms the function to return a BoxFuture
+/// Handles auth async
+pub async fn handle_auth() -> Option<User> {
+    // Gets credentials
+    let mut credentials: UserLogin = get_credentials();
+    // Tries to authenticate the user
+    let mut user = authenticate(credentials.email.as_str(), credentials.password.as_str()).await;
+    // If couldnt get the user, it will ask again
+    while user.is_none()
+    {
+        credentials = get_credentials();
+        user = authenticate(credentials.email.as_str(), credentials.password.as_str()).await;
+    }
+    // Finally returns the user that we get
+    Some(user.unwrap())
+}
+
+#[async_recursion] // This transforms the function to return a BoxFuture
+/// Authenticate the user by using the email and password and trying to log into the user's mojang account
+pub async fn authenticate(email: &str, password: &str) -> Option<User> {
+    // The payload to send with the post request
     let payload = serde_json::json!(
     {
         "agent" : {
@@ -52,15 +79,25 @@ pub fn authenticate(email: &str, password: &str) -> Option<User> {
         "password" : password
     });
 
-    // send payload here
-    match ureq::post("https://authserver.mojang.com/authenticate").send_json(payload) {
-        Ok(userinfo) => {
-            let userinfo_json: serde_json::Value =
-                userinfo.into_json().expect("Error parsing auth json");
+    // Sends an async request
+    let client = reqwest::Client::new();
+    let response = client.post("https://authserver.mojang.com/authenticate")
+    .json(&payload)
+    .send()
+    .await
+    .expect("Couldn\'t sign in, please try again!");
 
+    // Checks the status code of the response
+    match response.error_for_status() {
+        // if the response is okay (200) than save the user's name and token
+        Ok(res) => {
+            // Parse the response into json
+            let userinfo_json: serde_json::Value = res.json::<serde_json::Value>().await.expect("Error parsing auth json");
+            
+            // Get the access token from response json
             let access_token = userinfo_json["accessToken"].clone();
+            // Get the username from the response json
             let username = userinfo_json["selectedProfile"]["name"].clone();
-
             Some(User {
                 name: username.as_str().expect("Error parsing json").to_string(),
                 token: access_token
@@ -68,12 +105,11 @@ pub fn authenticate(email: &str, password: &str) -> Option<User> {
                     .expect("Error parsing json")
                     .to_string(),
             })
-        }
-        Err(ureq::Error::Status(code, _resp)) => {
-            println!("Got status {}", code);
-
-            handle_auth()
-        }
-        Err(_) => handle_auth(),
+        },
+        // Print any errors
+        Err(err) => {
+            println!("Got status {:?}", err.status().unwrap());
+            handle_auth().await
+        },
     }
 }
